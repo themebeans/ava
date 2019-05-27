c1edbd0990c8327979a07c3d8ff9837559eb27c3
Nikola <godaddy@Nikolas-MacBook-Pro.local>
2019-05-27 13:57:12 +0200

    content-product template updated

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -13,28 +13,28 @@
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @author  WooThemes
  * @package WooCommerce/Templates
- * @version 3.4.0
+ * @version 3.6.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
 }
 
 global $product;
 
 // Ensure visibility.
-if ( empty( $product ) || ! $product->is_visible() ) {
-	return;
+if (empty($product) || !$product->is_visible()) {
+    return;
 }
 ?>
-<article <?php wc_product_class(); ?>>
+<article <?php wc_product_class('', $product); ?>>
 	<?php
 	/**
 	 * Hook: woocommerce_before_shop_loop_item hook.
 	 *
 	 * @hooked woocommerce_template_loop_product_link_open - 10
 	 */
-	do_action( 'woocommerce_before_shop_loop_item' );
+	do_action('woocommerce_before_shop_loop_item');
 
 	/**
 	 * Hook: woocommerce_before_shop_loop_item_title hook.
@@ -42,14 +42,14 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 	 * @hooked woocommerce_show_product_loop_sale_flash - 10
 	 * @hooked woocommerce_template_loop_product_thumbnail - 10
 	 */
-	do_action( 'woocommerce_before_shop_loop_item_title' );
+	do_action('woocommerce_before_shop_loop_item_title');
 
 	/**
 	 * Hook: woocommerce_shop_loop_item_title hook.
 	 *
 	 * @hooked woocommerce_template_loop_product_title - 10
 	 */
-	do_action( 'woocommerce_shop_loop_item_title' );
+	do_action('woocommerce_shop_loop_item_title');
 
 	/**
 	 * Hook: woocommerce_after_shop_loop_item_title hook.
@@ -57,7 +57,7 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 	 * @hooked woocommerce_template_loop_rating - 5
 	 * @hooked woocommerce_template_loop_price - 10
 	 */
-	do_action( 'woocommerce_after_shop_loop_item_title' );
+	do_action('woocommerce_after_shop_loop_item_title');
 
 	/**
 	 * Hook: woocommerce_after_shop_loop_item hook.
@@ -65,6 +65,6 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 	 * @hooked woocommerce_template_loop_product_link_close - 5
 	 * @hooked woocommerce_template_loop_add_to_cart - 10
 	 */
-	do_action( 'woocommerce_after_shop_loop_item' );
+	do_action('woocommerce_after_shop_loop_item');
 	?>
-</article>
+	</article>
